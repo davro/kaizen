ed0c84b827b6f2aba4636ede132949df57a23a88
David Stevens <mail.davro@gmail.com>
2024-08-06 12:35:44 +0000

    Added nvim-vim alias and cleanup

--- setup.bash
+++ setup.bash
@@ -1,36 +1,32 @@
 #!bin/bash
+################################################################################
 #
 # Kaizen setup.bash
 #
 # Build and Configure 
 #
+################################################################################
 
 # Install Packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+#git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]; then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+fi
 
-# Install fzr (need to test)
-#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-#~/.fzf/install
-
-# Install Node
-#curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
-#sudo apt-get install -y nodejs
-
-#sudo apt-get install -y elinks
-
-
+# Nvim setup 
 unlink ~/.config/nvim
 #rm -rf ~/.local/share/nvim
 ln -s ~/workspace/kaizen/nvim-davro ~/.config/nvim
-
 ls -l ~/.config/nvim/
 
+################################################################################
+#
+# Define the lines to be added to .bashrc
+#
+################################################################################
 
-# Define the lines to be added
+# Nvim to binary
 alias_line="alias nvim='~/workspace/apps/nvim/nvim-linux64/bin/nvim'"
-bind_line='bind -x "\"\C-f\": bash ~/workspace/kaizen/tmux-sessionizer;"'
-
-# Check if the alias line exists in ~/.bashrc
 if ! grep -Fxq "$alias_line" ~/.bashrc; then
     echo "$alias_line" >> ~/.bashrc
     echo "Added alias line to ~/.bashrc"
@@ -38,7 +34,18 @@ else
     echo "Alias line already exists in ~/.bashrc"
 fi
 
-# Check if the bind line exists in ~/.bashrc
+# vim - nvim
+vim_alias_line="alias vim='nvim'"
+if ! grep -Fxq "$vim_alias_line" ~/.bashrc; then
+    echo "$vim_alias_line" >> ~/.bashrc
+    echo "Vim Nvim Added alias line to ~/.bashrc"
+else
+    echo "Vim Nvim Alias line already exists in ~/.bashrc"
+fi
+
+
+# Bind tmux-sessionizer
+bind_line='bind -x "\"\C-f\": bash ~/workspace/kaizen/tmux-sessionizer;"'
 if ! grep -Fxq "$bind_line" ~/.bashrc; then
     echo "$bind_line" >> ~/.bashrc
     echo "Added bind line to ~/.bashrc"
